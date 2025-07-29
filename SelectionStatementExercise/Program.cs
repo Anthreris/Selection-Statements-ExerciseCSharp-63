e2e98bb9611f1d2788e3811f2ad4b970b1cb8526
Garrett Chitwood <gman@acvwa.com>
2025-07-28 20:03:46 -0700

    Created a number guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1,101);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Guess a favorite number between 1 and 100:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour response: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour response: {response}");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct\nThe number was {favNumber}!");
+                }
+            }
+            
         }
     }
 }
